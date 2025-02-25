9c8c638d4a1452f9f03f6caf72779d54b8f8b8a4
Oliver Hines <olivermhines@gmail.com>
2025-02-24 20:59:54 -0800

    Fix formatting

--- src/urcl-ld.f90
+++ src/urcl-ld.f90
@@ -5,7 +5,7 @@ module urcl_ld
     public :: linkurcl
 
     type, public :: string
-        character(len=:), allocatable :: value !filename
+        character(:), allocatable :: value ! filename
     end type
 
     integer, parameter, public :: REPLACE_INST = 0
@@ -13,16 +13,18 @@ module urcl_ld
 
     type, public :: replacement
         integer :: type
-        character(len=:), allocatable :: originalValue
-        character(len=:), allocatable :: replacementValue
+        character(:), allocatable :: originalValue
+        character(:), allocatable :: replacementValue
     end type
 
 contains
-    function getline(end,unit) result(line)
+    function getline(end, unit) result(line)
         logical, intent(out) :: end
         integer, intent(in) :: unit
-        character(len=:), allocatable :: line
-        character(len=256) :: readline
+        character(:), allocatable :: line
+
+        character(256) :: readline
+
         line = ''
     2   read (unit, '(A)', advance='no', eor=3, end=999) readline
         line = line//readline
@@ -34,38 +36,42 @@ contains
     end
 
     function toupper(str) result(result)
-        character(len=*), intent(in) :: str
-        character(len=:), allocatable :: result
+        character(*), intent(in) :: str
+        character(:), allocatable :: result
         integer :: i
         
-        allocate(character(len=len(str)) :: result)
+        allocate(character(len(str)) :: result)
 
-        do i=1,len(str)
-            if (str(i:i)>='a'.and.str(i:i)<='z') then
-                result(i:i) = achar(iachar(str(i:i))-iachar('a')+iachar('A'))
+        do i = 1, len(str)
+            if (str(i:i) >= 'a' .and. str(i:i) <= 'z') then
+                result(i:i) = achar(iachar(str(i:i)) - iachar('a') + iachar('A'))
             else
                 result(i:i) = str(i:i)
             end if
         end do
     end
 
-    integer function findreplace(replacements,value,type) result(location)
+    function findreplace(replacements,value,type) result(location)
         type(replacement), intent(in) :: replacements(:)
-        character(len=*), intent(in) :: value
+        character(*), intent(in) :: value
+        integer :: location
+
         integer, intent(in) :: type
 
-        do location=1,size(replacements)
-            associate (current=>replacements(location))
-                if (current%type==type .and. toupper(current%originalValue)==value) return
+        do location = 1, size(replacements)
+            associate (current => replacements(location))
+                if (current%type == type .and. toupper(current%originalValue) == value) return
             end associate
         end do
         location = 0
     end function
 
-    integer function escapechar(character,fname,lnum) result(char)
+    function escapechar(character,fname,lnum) result(char)
         character(1), intent(in) :: character
-        character(len=*), intent(in) :: fname
+        character(*), intent(in) :: fname
         integer, intent(in) :: lnum
+        integer :: char
+
         select case (character)
         case ('n')
             char = 10
@@ -88,30 +94,31 @@ contains
         case ('\')
             char = iachar('\')
         case default
-            write(*,'(A)',advance='no') 'error: unknown escape sequence in '//fname//' on line '
-            write(*,'(I0)') lnum
+            write(*, '(A)', advance='no') 'error: unknown escape sequence in '//fname//' on line '
+            write(*, '(I0)') lnum
         end select
     end function
 
     pure function itoa(i)
         integer, intent(in) :: i
-        character(len=:), allocatable :: itoa
-        itoa = repeat(' ',12)
-        write(itoa,'(I12)') i
+        character(:), allocatable :: itoa
+
+        itoa = repeat(' ', 12)
+        write(itoa, '(I12)') i
         itoa = trim(adjustl(itoa))
     end function
 
-    function linkurcl(inputs,replacements,ascii) result(linked)
-        character(len=:), allocatable :: linked
+    function linkurcl(inputs, replacements, ascii) result(linked)
         type(string), intent(in) :: inputs(:)
         type(replacement), intent(in) :: replacements(:)
         logical, intent(in) :: ascii
+        character(:), allocatable :: linked
 
         integer :: i, j
         
-        character(len=:), allocatable :: defines, headers, line, instruction
-        character(len=:), allocatable :: lineend, substr
-        character(len=256) :: fname
+        character(:), allocatable :: defines, headers, line, instruction
+        character(:), allocatable :: lineend, substr
+        character(256) :: fname
         logical :: end
         integer :: unit, replacenum, idx, status, lnum, tmpi
 
@@ -128,14 +135,15 @@ contains
         headers = ''
         linked = ''
 
-        outer: do i=1,size(inputs)
-            open(newunit=unit,file=inputs(i)%value,status='old')
+        outer: &
+        do i = 1, size(inputs)
+            open(newunit=unit, file=inputs(i)%value, status='old')
 
-            idx = max(index(inputs(i)%value,'/',.true.),index(inputs(i)%value,'\',.true.))
-            fname = inputs(i)%value(idx+1:)
-            if (index(fname,'.')/=0) fname = fname(:index(fname,'.')-1)
-            do while (index(trim(fname),' ')/=0)
-                idx = index(trim(fname),' ')
+            idx = max(index(inputs(i)%value, '/', .true.), index(inputs(i)%value, '\', .true.))
+            fname = inputs(i)%value(idx + 1:)
+            if (index(fname, '.') /= 0) fname = fname(:index(fname, '.') - 1)
+            do while (index(trim(fname), ' ') /= 0)
+                idx = index(trim(fname), ' ')
                 fname(idx:idx) = '-'
             end do
             lnum = 0
@@ -146,52 +154,52 @@ contains
                 ! replace chars and strings
                 if (ascii) then
                     j = 1
-                    do while (j<=len(line))
-                        if (j<len(line)) then
-                            if (line(j:j+1)=='//') then
+                    do while (j <= len(line))
+                        if (j < len(line)) then
+                            if (line(j:j + 1) == '//') then
                                 exit
                             end if
                         end if
-                        if (line(j:j)=='''') then
+                        if (line(j:j) == '''') then
                             j = j + 1
-                            if (line(j:j)=='\') then
-                                tmpi = escapechar(line(j+1:j+1),fname,lnum)
-                                line(j-1:j-1) = ' '
-                                if (tmpi<100) then
+                            if (line(j:j) == '\') then
+                                tmpi = escapechar(line(j + 1:j + 1), fname, lnum)
+                                line(j - 1:j - 1) = ' '
+                                if (tmpi < 100) then
                                     line(j:j) = ' '
                                 else
-                                    line(j:j) = achar(tmpi/100+48)
+                                    line(j:j) = achar(tmpi / 100 + 48)
                                 end if
-                                line(j+1:j+1) = achar(mod(tmpi,100)/10+48)
-                                line(j+2:j+2) = achar(mod(tmpi,10)+48)
+                                line(j + 1:j + 1) = achar(mod(tmpi, 100)/ 10 + 48)
+                                line(j + 2:j + 2) = achar(mod(tmpi, 10) + 48)
                                 j = j + 3
                             else
                                 tmpi = iachar(line(j:j))
-                                if (tmpi<100) then
-                                    line(j-1:j-1) = ' '
+                                if (tmpi < 100) then
+                                    line(j - 1:j - 1) = ' '
                                 else
-                                    line(j-1:j-1) = achar(tmpi/100+48)
+                                    line(j - 1:j - 1) = achar(tmpi / 100 + 48)
                                 end if
-                                line(j:j) = achar(mod(tmpi,100)/10+48)
-                                line(j+1:j+1) = achar(mod(tmpi,10)+48)
+                                line(j:j) = achar(mod(tmpi, 100)/ 10 + 48)
+                                line(j + 1:j + 1) = achar(mod(tmpi, 10) + 48)
                                 j = j + 2
                             end if
-                        else if (line(j:j)=='"') then
-                            lineend = line(:j-1) !ironic
+                        else if (line(j:j) == '"') then
+                            lineend = line(:j - 1) !ironic
                             j = j + 1
                             substr = ''
-                            do while (line(j:j)/='"')
-                                if (line(j:j)=='\') then
+                            do while (line(j:j) /= '"')
+                                if (line(j:j) == '\') then
                                     j = j + 1
-                                    tmpi = escapechar(line(j:j),fname,lnum)
+                                    tmpi = escapechar(line(j:j), fname, lnum)
                                 else
                                     tmpi = iachar(line(j:j))
                                 end if
                                 substr = substr//itoa(tmpi)//' '
                                 j = j + 1
                             end do
-                            line = lineend//substr//line(j+1:)
-                            j = len(lineend)+len(substr)+1
+                            line = lineend//substr//line(j + 1:)
+                            j = len(lineend) + len(substr) + 1
                         else
                             j = j + 1
                         end if
@@ -200,27 +208,27 @@ contains
 
                 if (end) cycle outer
                 if (len(line) < 1) cycle
-                if (line(:1)=='!') then
+                if (line(:1) == '!') then
                     !symbol declaration
                     symbolptr = symbolptr + 1
-                    if (symbolptr>size(symbols)) then
-                        allocate(tmp(size(symbols)+64))
+                    if (symbolptr > size(symbols)) then
+                        allocate(tmp(size(symbols) + 64))
                         tmp(:size(symbols)) = symbols(:)
-                        call move_alloc(tmp,symbols)
+                        call move_alloc(tmp, symbols)
 
-                        allocate(tmp(size(association)+64))
+                        allocate(tmp(size(association) + 64))
                         tmp(:size(association)) = association(:)
-                        call move_alloc(tmp,association)
+                        call move_alloc(tmp, association)
                     end if
                     symbols(symbolptr)%value = line
-                    line = trim(adjustl(getline(end,unit)))
+                    line = trim(adjustl(getline(end, unit)))
                     lnum = lnum + 1
 
                     if (end) then
                         print'(A)','error: end of file following symbol declaration'
                         stop
                     end if
-                    if (line(:1)/='.') then
+                    if (line(:1) /= '.') then
                         print'(A)','error: symbol declaration not immediately followed by a label'
                         stop
                     end if
@@ -228,41 +236,41 @@ contains
                     association(symbolptr)%value = line
 
                     linked = linked//line//achar(10)
-                else if (line(:1)=='.') then
+                else if (line(:1) == '.') then
                     linked = linked//'.'//trim(fname)//'_'//line(2:)//achar(10)
                 else
                     !replace instruction
-                    idx = index(line,' ')
-                    if (idx==0) idx = len(line)+1
-                    instruction = toupper(line(:idx-1))
+                    idx = index(line, ' ')
+                    if (idx == 0) idx = len(line) + 1
+                    instruction = toupper(line(:idx - 1))
                     line = line(idx:)
 
-                    replacenum = findreplace(replacements,instruction,REPLACE_INST)
+                    replacenum = findreplace(replacements, instruction, REPLACE_INST)
                     if (replacenum /= 0) then
                         instruction = replacements(replacenum)%replacementValue
                     end if
 
                     !apply other replacements
                     !replace ports
-                    if (index(line,'%')/=0) then
-                        if (index(line,'//')==0.or.index(line,'//')>index(line,'%')) then
-                            lineend = line(index(line,'%'):)
+                    if (index(line, '%') /= 0) then
+                        if (index(line, '//') == 0 .or. index(line, '//') > index(line, '%')) then
+                            lineend = line(index(line, '%'):)
                             
-                            idx = index(lineend,' ')
-                            if (idx == 0) idx = len(lineend)+1
-                            substr = toupper(lineend(2:idx-1))
+                            idx = index(lineend, ' ')
+                            if (idx == 0) idx = len(lineend) + 1
+                            substr = toupper(lineend(2:idx - 1))
                             lineend = lineend(idx:)
-                            replacenum = findreplace(replacements,substr,REPLACE_PORT)
-                            if (replacenum/=0) then
-                                line = line(:index(line,'%')-1)//'%'//replacements(replacenum)%replacementValue//lineend
+                            replacenum = findreplace(replacements, substr, REPLACE_PORT)
+                            if (replacenum /= 0) then
+                                line = line(:index(line, '%') - 1)//'%'//replacements(replacenum)%replacementValue//lineend
                             end if
                         end if
                     end if
 
                     !replace labels
-                    do while (index(line,'.')/=0)
+                    do while (index(line, '.') /= 0)
                         ! check if in string or comment
-                        idx = index(line,'.')
+                        idx = index(line, '.')
                         if (idx /= 1) then
                             if (line(idx - 1:idx - 1) /= ' ') then
                                 exit
@@ -270,38 +278,39 @@ contains
                         end if
                         status = 0
                         j = 1
-                        do while (j<idx)
-                            if (status==2) then ! inside single quote
-                                if (line(j:j)=='\') then
+                        do while (j < idx)
+                            if (status == 2) then ! inside single quote
+                                if (line(j:j) == '\') then
                                     status = 2
                                 else
                                     status = 1
                                 end if
-                            else if (status==1) then
-                                if (line(j:j)/='''') then
-                                    write(*,'(A)',advance='no') 'error: missing closing single quote in '//trim(fname)//' on line '
-                                    write(*,'(I0)') lnum
+                            else if (status == 1) then
+                                if (line(j:j) /= '''') then
+                                    write(*, '(A)', advance='no') 'error: missing closing single quote in '//trim(fname)//&
+                                                                    ' on line '
+                                    write(*, '(I0)') lnum
                                 end if
                                 status = 0
-                            else if (status==4) then
-                                if (line(j:j)=='"') then
+                            else if (status == 4) then
+                                if (line(j:j) == '"') then
                                     status = 0
                                 endif
-                            else if (status==0) then
-                                if (line(j:j)=='"') then
+                            else if (status == 0) then
+                                if (line(j:j) == '"') then
                                     status = 4
-                                else if (line(j:j)=='''') then
+                                else if (line(j:j) == '''') then
                                     status = 2
-                                else if (line(j:j+1)=='//') then
+                                else if (line(j:j+1) == '//') then
                                     status = 5
                                     exit
                                 end if
                             end if
                             j = j + 1
                         end do
-                        if (status==0) then
-                            lineend = line(idx+1:)
-                            line = line(:idx-1)
+                        if (status == 0) then
+                            lineend = line(idx + 1:)
+                            line = line(:idx - 1)
                             line = line//achar(17)//trim(fname)//'_'//lineend
                         else
                             line(idx:idx) = achar(17)
@@ -328,11 +337,11 @@ contains
 
         linked = headers//defines//linked
         ! replace symbol references
-        do i=1,symbolptr
-            associate (symbol=>symbols(i)%value)
-                do while (index(linked,symbol//' ')/=0.or.index(linked,symbol//achar(10))/=0)
-                    idx = max(index(linked,symbol//' '),index(linked,symbol//achar(10)))
-                    linked = linked(:idx-1)//association(i)%value//linked(idx+len(symbol):)
+        do i = 1,symbolptr
+            associate (symbol => symbols(i)%value)
+                do while (index(linked, symbol//' ') /= 0 .or. index(linked, symbol//achar(10)) /= 0)
+                    idx = max(index(linked, symbol//' '), index(linked, symbol//achar(10)))
+                    linked = linked(:idx - 1)//association(i)%value//linked(idx + len(symbol):)
                 end do
             end associate
         end do
